310fcd95babf13d37255d7178a38ed4c424466e3
Ludicard <sergio.masin@udla.edu.ec>
2025-07-08 21:14:37 -0500

    Conectado el repositorio con la base de datos

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -1,60 +1,63 @@
-﻿using People.Models;
+﻿using SQLite;
+using People.Models;
+using System;
+using System.Collections.Generic;
 
-namespace People;
-
-public class PersonRepository
+namespace People
 {
-    string _dbPath;
-
-    public string StatusMessage { get; set; }
-
-    // TODO: Add variable for the SQLite connection
-
-    private void Init()
+    public class PersonRepository
     {
-        // TODO: Add code to initialize the repository         
-    }
+        private SQLiteConnection conn;
+        private string _dbPath;
 
-    public PersonRepository(string dbPath)
-    {
-        _dbPath = dbPath;                        
-    }
+        public string StatusMessage { get; set; }
 
-    public void AddNewPerson(string name)
-    {            
-        int result = 0;
-        try
+        public PersonRepository(string dbPath)
         {
-            // TODO: Call Init()
-
-            // basic validation to ensure a name was entered
-            if (string.IsNullOrEmpty(name))
-                throw new Exception("Valid name required");
-
-            // TODO: Insert the new person into the database
-            result = 0;
-
-            StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            _dbPath = dbPath;
         }
 
-    }
+        private void Init()
+        {
+            if (conn != null)
+                return;
 
-    public List<Person> GetAllPeople()
-    {
-        // TODO: Init then retrieve a list of Person objects from the database into a list
-        try
-        {
-            
-        }
-        catch (Exception ex)
-        {
-            StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Person>();
         }
 
-        return new List<Person>();
+        public void AddNewPerson(string name)
+        {
+            int result = 0;
+            try
+            {
+                Init();
+
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("Valid name required");
+
+                result = conn.Insert(new Person { Name = name });
+                StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
+            }
+        }
+
+        public List<Person> GetAllPeople()
+        {
+            try
+            {
+                Init();
+                return conn.Table<Person>().ToList();
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return new List<Person>();
+        }
     }
 }
